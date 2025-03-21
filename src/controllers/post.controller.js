33b2f3e0e242fd72776166553f174c246ba13c63
Vishal Kumar Gupta <vishaaal05@gmail.com>
2025-03-21 08:16:14 +0530

    refactor deletePost and updatePost functions for improved error handling and validation

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -37,68 +37,75 @@ const createPost = async (req, res) => {
 
 const deletePost = async (req, res) => {
   try {
-    const {id} = req.params;
+    const { id } = req.params;
 
     if (!id) {
-      return res.status(400).json({ 
+      return res.status(400).json({
         success: false,
-        message: "Post ID is required" 
+        message: "Post ID is required",
       });
     }
 
     const response = await postService.deletePostService(id);
-   return res.status(200).json({
+    return res.status(200).json({
       success: true,
-      ...response
+      ...response,
     });
   } catch (error) {
     console.error("Error deleting post:", error.message);
-    
+
     if (error.message === "Invalid post ID") {
       return res.status(400).json({
         success: false,
-        message: error.message
+        message: error.message,
       });
     }
-    
+
     if (error.message === "Post not found") {
       return res.status(404).json({
         success: false,
-        message: error.message
+        message: error.message,
       });
     }
 
     return res.status(500).json({
       success: false,
-      message: "Internal server error"
+      message: "Internal server error",
     });
   }
 };
 
-const updatePost = async (req, res) =>{
+const updatePost = async (req, res) => {
   try {
-    const {id} = req.params;
-    const {title, content } = req.body;
+    const { id } = req.params;
+    const { title, content } = req.body;
 
-    if(!id){
+    if (!id) {
       return res.status(400).json({
         success: false,
-        message: "Post ID is required" 
-      })
+        message: "Post ID is required",
+      });
+    }
+
+    if (!title || !content) {
+      return res.status(400).json({
+        success: false,
+        message: "Title or content required",
+      });
     }
 
     const response = await postService.updatePostService(id, title, content);
 
     return res.status(200).json({
-      success:true,
-      message:"post updated successfully"
-    })
+      success: true,
+      message: "post updated successfully",
+    });
   } catch (error) {
-    console.error("Error while updating post", error.message)
+    console.error("Error while updating post", error.message);
     return res.status(500).json({
-      success:false,
-      message:error.message
-    })
+      success: false,
+      message: error.message,
+    });
   }
-}
+};
 module.exports = { getPosts, createPost, deletePost, updatePost };
