9150e504dbedf295465df005313d0b876acdd03a
Vishal Kumar Gupta <vishaaal05@gmail.com>
2025-03-27 15:23:11 +0530

    add route to fetch posts by author ID

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getPosts, createPost, deletePost, updatePost, getPostById, updatePostStatus } = require("../controllers/post.controller");
+const { getPosts, createPost, deletePost, updatePost, getPostById, updatePostStatus, getPostsByAuthorId } = require("../controllers/post.controller");
 const authMiddleware = require("../middlewares/auth.middleware");
 const router = express.Router();
 
@@ -9,5 +9,6 @@ router.delete("/:id",authMiddleware, deletePost);
 router.put("/:id", authMiddleware, updatePost)
 router.get("/:id", getPostById);
 router.patch("/:id/status", authMiddleware, updatePostStatus);
+router.get("/author/:authorId", getPostsByAuthorId);
 
 module.exports = router;
