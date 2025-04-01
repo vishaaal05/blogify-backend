ff296356940f052dcb407335133c52b1376ef032
Vishal Kumar Gupta <vishaaal05@gmail.com>
2025-04-01 16:45:41 +0530

    add route for favorites to the main application

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const postRoutes = require("./routes/post.routes");
 const userRoutes = require("./routes/user.routes");
 const commentRoutes = require("./routes/comment.routes");
 const likeRoutes = require("./routes/like.routes");
+const favoriteRoutes = require("./routes/favorite.routes");
 
 const app = express();
 app.use(express.json());
@@ -12,6 +13,7 @@ app.use(cors());
 app.use("/v1/api/posts", postRoutes);
 app.use("/v1/api/users", userRoutes);
 app.use("/v1/api/comments", commentRoutes);
-app.use("/v1/api/likes", likeRoutes)
+app.use("/v1/api/likes", likeRoutes);
+app.use("/v1/api/favorites", favoriteRoutes);
 
 module.exports = app;
