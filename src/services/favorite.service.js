4d2ba84008ed5f5b3c469e967906203e8a0b9585
Vishal Kumar Gupta <vishaaal05@gmail.com>
2025-04-01 16:46:39 +0530

    add favorite service for toggling favorites and fetching user favorites

---
+++ src/services/favorite.service.js
@@ -0,0 +1,54 @@
+const prisma = require("../config/db");
+
+const toggleFavorite = async (userId, postId) => {
+  const existingFavorite = await prisma.favorite.findUnique({
+    where: {
+      userId_postId: {
+        userId,
+        postId,
+      },
+    },
+  });
+
+  if (existingFavorite) {
+    // If favorite exists, remove it
+    await prisma.favorite.delete({
+      where: {
+        userId_postId: {
+          userId,
+          postId,
+        },
+      },
+    });
+    return { message: "Post removed from favorites" };
+  }
+
+  // If favorite doesn't exist, create it
+  await prisma.favorite.create({
+    data: {
+      userId,
+      postId,
+    },
+  });
+  return { message: "Post added to favorites" };
+};
+
+const getUserFavorites = async (userId) => {
+  return await prisma.favorite.findMany({
+    where: { userId },
+    include: {
+      post: {
+        include: {
+          author: true,
+          likes: true,
+          comments: true,
+        },
+      },
+    },
+  });
+};
+
+module.exports = {
+  toggleFavorite,
+  getUserFavorites,
+};
