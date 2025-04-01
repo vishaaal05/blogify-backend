cb017809cb8ad4af15854150c88d1249903a4b6b
Vishal Kumar Gupta <vishaaal05@gmail.com>
2025-04-01 22:21:18 +0530

    add cookie-parser middleware for handling cookies

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const postRoutes = require("./routes/post.routes");
 const userRoutes = require("./routes/user.routes");
 const commentRoutes = require("./routes/comment.routes");
@@ -9,6 +10,7 @@ const favoriteRoutes = require("./routes/favorite.routes");
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 app.use("/v1/api/posts", postRoutes);
 app.use("/v1/api/users", userRoutes);
