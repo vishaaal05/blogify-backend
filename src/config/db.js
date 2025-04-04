fd6456281546bb8c8a9b0ec3850ed2cdde3bc264
Vishal Kumar Gupta <vishaaal05@gmail.com>
2025-04-04 21:46:23 +0530

    format db.js for consistency and improve connection logging

--- src/config/db.js
+++ src/config/db.js
@@ -1,3 +1,14 @@
-const { PrismaClient } = require("@prisma/client");
+const { PrismaClient } = require('@prisma/client');
+
 const prisma = new PrismaClient();
+
+// Test the connection
+prisma.$connect()
+  .then(() => {
+    console.log('Successfully connected to the database');
+  })
+  .catch((error) => {
+    console.error('Failed to connect to the database:', error);
+  });
+
 module.exports = prisma;
